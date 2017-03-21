fc91066a0f29468824f2d92b00b0be3d6d9d6ba8
Sarah Buchanan <sbuchanan@codeschool.com>
2017-03-21 14:51:17 -0400

    Created Java code to update the text of the TextView when the app is running

--- CandyCoded/app/src/main/java/com/codeschool/candycoded/MainActivity.java
+++ CandyCoded/app/src/main/java/com/codeschool/candycoded/MainActivity.java
@@ -1,7 +1,8 @@
 package com.codeschool.candycoded;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)this.findViewById(R.id.text_view_title);
+        textView.setText(R.string.products_title);
     }
 }
